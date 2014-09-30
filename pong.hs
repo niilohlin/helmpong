5c4ac6b0b286e5c7ae2b767c6856a5f5fd0ee2d8
niil <niil.94@hotmail.com>
2014-09-30 17:29:01 +0200

    some change

--- pong.hs
+++ pong.hs
@@ -5,8 +5,8 @@ import qualified FRP.Helm.Keyboard as Keyboard
 import qualified FRP.Helm.Window as Window
 
 
-data Player = Player { mx :: Double, my :: Double, speed :: Double}
-data Ball   = Ball   { ballX :: Double, ballY :: Double, dx :: Double, dy :: Double}
+data Player = Player { mx, my, speed , paddleWidth, paddleHeight :: Double}
+data Ball   = Ball   { ballX , ballY , dx , dy, rad :: Double}
 
 type Pos = (Double, Double)
 
@@ -21,18 +21,20 @@ playerPos p = (mx p, my p)
 inside :: (Ord a, Fractional a) => a -> a -> Bool
 inside point wall = -wall / 2 <= point && point <= wall / 2
 
-ballSignal :: SignalGen (Signal Pos)
+initBallState :: Ball
+initBallState    = Ball { ballX = 0, ballY = 0, dx = 3, dy = 2, rad = 5}
+
+ballSignal :: SignalGen (Signal Ball)
 ballSignal = mdo
-    ballP <- (\b -> (ballX b, ballY b)) <~ do
+    ballP <- do
         aiPos <- aiSignal ballP'
         plPos <- playerSignal
         transfer2 initBallState updater aiPos plPos
-    ballP' <- delay (0, 0)  ballP
+    ballP' <- delay initBallState  ballP
     return ballP
         where
-            initBallState    = Ball { ballX = 0, ballY = 0, dx = 3, dy = 2}
-            updater :: Pos -> Pos -> Ball -> Ball
-            updater (x1, y1) (x2, y2) myball@(Ball{ballX = x, ballY = y, dx = dx', dy = dy'}) =
+            updater :: Player -> Player -> Ball -> Ball
+            updater Player{mx = x1, my = y1} Player{mx = x2, my = y2} myball@(Ball{ballX = x, ballY = y, dx = dx', dy = dy'}) =
                 myball{ ballX = newx, ballY = newy,
                         dx = if not (inside newx width) || colliding then
                             (-1) * (dx' + (signum dx') * 0.5)
@@ -47,31 +49,31 @@ ballSignal = mdo
                     newx = x + dx'
                     newy = y + dy'
 
-aiSignal :: Signal Pos ->  SignalGen (Signal Pos)
-aiSignal ballP = playerPos <~ transfer initial newState ballP
+aiSignal :: Signal Ball ->  SignalGen (Signal Player)
+aiSignal ballP =  transfer initial newState ballP
         where
-            initial = Player { mx =  375, my = 0, speed = 2.5}
-            newState :: Pos -> Player -> Player
-            newState (_, by) ai' = ai'{my = min (max newPos (-height / 2 + 50))
+            initial = Player { mx =  375, my = 0, speed = 2.5, paddleWidth = 20, paddleHeight = 100}
+            newState :: Ball -> Player -> Player
+            newState Ball{ballY =  by} ai' = ai'{my = min (max newPos (-height / 2 + 50))
                                                 (height / 2 - 50)
                                                 }
                   where newPos = (my ai') + signum (by - my ai') * speed ai'
 
-playerSignal :: SignalGen (Signal Pos)
-playerSignal = playerPos <~ (foldp newState initState Keyboard.arrows)
+playerSignal :: SignalGen (Signal Player)
+playerSignal = foldp newState initState Keyboard.arrows
     where
-        initState = Player { mx = -375, my = 0, speed = 2.5}
+        initState = Player { mx = -375, my = 0, speed = 2.5, paddleWidth = 20, paddleHeight = 100}
         newState :: (Int, Int) -> Player -> Player
         newState (_, dir) me = me {
                 my = min (max newPos (-height / 2 + 50)) (height / 2 - 50)
             }
             where newPos = my me + (realToFrac dir) * speed me
 
-render :: (Int, Int) -> Pos -> Pos -> Pos -> Element
-render (w, h) (playerX, playerY) (aiX, aiY) (x, y) =
-    centeredCollage w h [move (playerX, playerY) $ filled white $ rect 20 100,
-                         move (aiX, aiY)         $ filled white $ rect 20 100,
-                         move (x, y)             $ filled white $ rect 10 10
+render :: (Int, Int) -> Player -> Player -> Ball -> Element
+render (w, h) player ai ball  =
+    centeredCollage w h [move (mx player, my player)   $ filled white $ rect (paddleWidth player) (paddleHeight player),
+                         move (mx ai, my ai)           $ filled white $ rect (paddleWidth ai) (paddleHeight ai),
+                         move (ballX ball, ballY ball) $ filled white $ circle (rad ball)
                         ]
 
 main :: IO ()
